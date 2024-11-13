2aee8f1a350f7327e360567e68ef5cda6345dde9
facundo-rodriguez <rodriguez.facundo95@hotmail.com>
2024-11-13 17:20:04 -0300

    agregué cors

--- src/app.js
+++ src/app.js
@@ -2,8 +2,17 @@ import express from 'express';
 // import config from './config.js';
 import categorias from './modulos/categorias/ruta.js';
 import productos from './modulos/productos/ruta.js';
+import cors from 'cors'
 
 const app = express();
+
+const corsOptions = {
+    origin: 'https://mi-dominio.com',  // Permitir solo este origen
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],          // Permitir solo ciertos métodos HTTP
+    allowedHeaders: ['Content-Type'],  // Permitir ciertos encabezados
+  };
+
+app.use(cors());
 
 app.use(express.json());
 
