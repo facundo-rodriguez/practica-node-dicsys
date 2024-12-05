9295036ef3ed6d09ed0c72aefc5de756855f421b
facundo-rodriguez <72040590+facundo-rodriguez@users.noreply.github.com>
2024-12-05 02:02:01 -0300

    Update cookie login

--- src/modulos/login/ruta.js
+++ src/modulos/login/ruta.js
@@ -22,13 +22,7 @@ router.post('/', isNotAuthenticated, async (req, res) => {
         const token= await createToken(body, user);
 
         res
-        .cookie('access_token', token,{
-           // httpOnly: true, //la cookie solo se puede acceder en el servidor
-            sameSite: 'None', //la cookie solo se puede acceder en el mismo dominio, lax, strict
-            secure: false,
-            maxAge: 1000*60*60, // la cookie tiene un tiempo de validez de 1h
-
-        })
+        .cookie('access_token', token)
         .json({ message: 'Login exitoso', token: token });
         // res.send(result);
     
