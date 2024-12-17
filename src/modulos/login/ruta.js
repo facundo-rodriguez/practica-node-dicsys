19e6a412ab258eb3d61798decdee74c19d2373f1
facundo-rodriguez <72040590+facundo-rodriguez@users.noreply.github.com>
2024-12-17 06:30:58 -0300

    probando con domain otra vez

--- src/modulos/login/ruta.js
+++ src/modulos/login/ruta.js
@@ -27,7 +27,7 @@ router.post('/', isNotAuthenticated, async (req, res) => {
             httpOnly: true, //la cookie solo se puede acceder en el servidor
             sameSite: 'none', //la cookie solo se puede acceder en el mismo dominio, Necesario para que funcione entre dominios diferentes
             secure: true,
-            //domain: 'practica-angular-dicsys.netlify.app',
+            domain: 'practica-node-dicsys.onrender.com',
             maxAge: 1000*60*60, // la cookie tiene un tiempo de validez de 1h
                 })
         .json({ message: 'Login exitoso', token: token, username: user.username, rol: user.rol });
